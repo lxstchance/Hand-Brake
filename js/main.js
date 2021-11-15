4ffb53697bb68acfcb40648f3367e92006984324
lxstchance <kirpichnikovstas@gmail.com>
2021-11-15 14:09:58 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(function () {
 
+  $('.menu__list-btn').on('click', function () {
+    $('.menu__list-btn').toggleClass('menu__list-btn--active');
+    $('.menu__list').toggleClass('menu__list--active');
+  })
+
 })
